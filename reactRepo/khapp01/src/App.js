225e88e16be77ac9a77a4a2bbf84543aec3ab1fc
DESKTOP-U4JO88Q\user1 <corey5361@nate.com>
2023-12-06 17:56:51 +0900

    reactRepo 첫 수업 진행

---
+++ reactRepo/khapp01/src/App.js
@@ -0,0 +1,37 @@
+
+import './App.css';
+import Abc from './component/KhBtn.js';
+
+function App() {
+  return (
+    <>
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    <Abc />
+    </>
+  );
+}
+
+export default App;
---
+++ reactRepo/khapp01/src/component/KhBtn.js
@@ -0,0 +1,12 @@
+function f01(){
+    return(
+        <div>
+            <button>KH버튼</button>
+            <button>작성하기</button>
+            <button>수정하기</button>
+            <button>삭제하기</button>
+        </div>
+    );
+}
+
+export default f01;
