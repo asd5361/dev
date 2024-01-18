46a1a82518156d12b7dbc9af4ad119b6c255c270
DESKTOP-RO4OUHM\user1 <corey5361@nate.com>
2024-01-18 15:35:20 +0900

    로더랑 빌더

--- springRepo/springPrj99/src/main/java/com/kh/app/board/controller/BoardApiController.java
+++ springRepo/springPrj99/src/main/java/com/kh/app/board/controller/BoardApiController.java
@@ -1,9 +1,13 @@
 package com.kh.app.board.controller;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,27 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.app.board.service.BoardService;
 import com.kh.app.board.vo.BoardVo;
-import com.kh.app.member.vo.MemberVo;
+import com.kh.app.msg.controller.MsgController;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequestMapping("api/board")
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin("*")
+@Slf4j
 public class BoardApiController {
 
 	private final BoardService service;
 	
-	//게시글 목록 조회
+	//게시글 목록 조회 + logger (로거) 예제로 사용
 	@GetMapping("list")
 	public List<BoardVo>list() throws InterruptedException{
-		List<BoardVo> BoardVoList = service.list();
-		
-		for (BoardVo boardVo : BoardVoList) {
-			System.out.println(boardVo);
-		}
-		return BoardVoList;
+		String str = ":"; 
+		log.info("BoardController 클래스의 list 메소드 호출됨. {}", str); //이런 방식으로 사용 가능
+//		service.list();
+		return null;
 	}
 	
 	@PostMapping("write")
--- springRepo/springPrj99/src/main/java/com/kh/app/board/vo/BoardVo.java
+++ springRepo/springPrj99/src/main/java/com/kh/app/board/vo/BoardVo.java
@@ -1,8 +1,10 @@
 package com.kh.app.board.vo;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class BoardVo {
 
 	private String no;
